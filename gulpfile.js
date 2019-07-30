ba84d8a8f5234eadf8d9c0506a6ef41214ea88fa
Davide Ciulla <davide.ciulla@ticino.com>
2019-07-30 23:05:16 +0200

    countless improvements

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,16 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
-const uglifycss = require('gulp-uglifycss');
 const autoprefixer = require('gulp-autoprefixer');
 const sourcemaps = require('gulp-sourcemaps');
 
 function sassTask() {
   return gulp
-    .src('./styles/main.sass')
-    .pipe(sourcemaps.init())
-      .pipe(sass().on('error', sass.logError))
-      .pipe(autoprefixer())
-      .pipe(uglifycss({uglyComments: true,}),)
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('./styles'));
+	.src('./styles/main.sass')
+	.pipe(sourcemaps.init())
+		.pipe(sass({outputStyle: "compressed"}).on('error', sass.logError))
+		.pipe(autoprefixer())
+	.pipe(sourcemaps.write('.'))
+	.pipe(gulp.dest('./styles'));
 }
 
 function watchTask() {
