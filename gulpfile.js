fb9113be89c3b064fb781db11818805277fa7b32
Davide Ciulla <davide.ciulla@ticino.com>
2019-08-13 01:33:57 +0200

    end of day commit

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const sourcemaps = require('gulp-sourcemaps');
+const connect = require('gulp-connect');
 
 function sassTask() {
   return gulp
@@ -10,13 +11,21 @@ function sassTask() {
 		.pipe(sass({outputStyle: "compressed"}).on('error', sass.logError))
 		.pipe(autoprefixer())
 	.pipe(sourcemaps.write('.'))
-	.pipe(gulp.dest('./styles'));
+	.pipe(gulp.dest('./styles'))
+	.pipe(connect.reload());
 }
 
 function watchTask() {
-  gulp.watch('./styles/**/*.sass', sassTask);
+	gulp.watch('./styles/**/*.sass', sassTask);
+}
+
+function serverTask() {
+	connect.server({
+		root: '.',
+		livereload: true
+	});
 }
 
 exports.sassTask = sassTask;
 exports.watchTask = watchTask;
-exports.default = gulp.parallel(sassTask, watchTask);
+exports.default = gulp.parallel(sassTask, gulp.parallel(serverTask, watchTask));
