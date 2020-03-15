13be2d839ca4c9a6b1eab5ed8b24c45da877cd78
Davide <davide.ciulla@ticino.com>
2020-03-15 23:20:11 +0100

    added visibility field to projects.json

--- src/js/populate-portfolio.js
+++ src/js/populate-portfolio.js
@@ -23,35 +23,41 @@ async function fetchData(url) {
 
 		for (project in json.projects) {
 
-			let outerDiv = document.createElement('div');
-			let innerDiv = document.createElement('div');
+			console.log(json.projects[project].visibility)
 
-			outerDiv.className += 'column is-one-quarter-widescreen is-one-third-desktop is-half-tablet columnHide columnShow';
-			innerDiv.className += 'categories'
-			innerDiv.style.backgroundImage = 'url(./img/portfolio/projects/' + json.projects[project].thumbnail + ')';
-			innerDiv.dataset.index = project;
+			if (json.projects[project].visibility === 'public') {
 
-			innerDiv.addEventListener('click', (e) => {
-				let index = e.target.dataset.index;
-				openModal(json.projects[index].title, json.projects[index].description, json.projects[index].categories, json.projects[index].technologies, json.projects[index].link, json.projects[index].presentation)
-			});
-
-			projectsContainer.appendChild(outerDiv);
-			outerDiv.appendChild(innerDiv);
-
-			for (category in json.projects[project].categories) {
-				let label = document.createElement('p');
-				let workCategory = json.projects[project].categories[category];
-				label.innerHTML = workCategory;
-				if (workCategory === 'UI/UX') {
-					label.classList.add('ux');
-					outerDiv.classList.add('ux');
-				} else {
-					label.classList.add(workCategory.toLowerCase());
-					outerDiv.classList.add(workCategory.toLowerCase());
-
+				let outerDiv = document.createElement('div');
+				let innerDiv = document.createElement('div');
+	
+				outerDiv.className += 'column is-one-quarter-widescreen is-one-third-desktop is-half-tablet columnHide columnShow';
+				innerDiv.className += 'categories'
+				innerDiv.style.backgroundImage = 'url(./img/portfolio/projects/' + json.projects[project].thumbnail + ')';
+				innerDiv.dataset.index = project;
+	
+				innerDiv.addEventListener('click', (e) => {
+					let index = e.target.dataset.index;
+					openModal(json.projects[index].title, json.projects[index].description, json.projects[index].categories, json.projects[index].technologies, json.projects[index].link, json.projects[index].presentation)
+				});
+	
+				projectsContainer.appendChild(outerDiv);
+				outerDiv.appendChild(innerDiv);
+	
+				for (category in json.projects[project].categories) {
+					let label = document.createElement('p');
+					let workCategory = json.projects[project].categories[category];
+					label.innerHTML = workCategory;
+					if (workCategory === 'UI/UX') {
+						label.classList.add('ux');
+						outerDiv.classList.add('ux');
+					} else {
+						label.classList.add(workCategory.toLowerCase());
+						outerDiv.classList.add(workCategory.toLowerCase());
+	
+					}
+					innerDiv.appendChild(label);
 				}
-				innerDiv.appendChild(label);
+
 			}
 
 		}
